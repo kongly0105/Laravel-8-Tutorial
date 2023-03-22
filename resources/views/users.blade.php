60569809fde1c84788ed1254475ba642291e9374
KONG LY <paly.kong@z1fsc.com>
2023-03-22 13:17:56 +0700

    Laravel 8 Tutorial - What is middleware?

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,23 +1,2 @@
-<h1>User login</h1>
+<h1>This is User login</h1>
 
-{{--
-    {{$errors}}
-    @if($errors->any())
-    @foreach($errors->all() as $err)
-    <li>{{$err}}</li>
-    @endforeach
-    @endif
---}}
-
-<form action="users" method="POST">
-    @csrf
-    <input type="text" name="username" placeholder="enter user id" /> <br>
-    <span style="color: red">
-        @error('username'){{$message}}@enderror
-    </span> <br>
-    <input type="password" name="userpassword" placeholder="enter user password" /> <br>
-    <span style="color: red">
-        @error('userpassword'){{$message}}@enderror
-    </span> <br>
-    <button type="submit">Login</button>
-</form>
--- routes/web.php
+++ routes/web.php
@@ -5,20 +5,24 @@ use App\Http\Controllers\UsersController;
 
 /*
 |--------------------------------------------------------------------------
-| Laravel 8 Tutorial - Form Validation
+| Laravel 8 Tutorial - What is middleware?
 |--------------------------------------------------------------------------
-|---Laravel Form Validation----
-|   Use Validation
-|   Show error massage
-|   Error with every field
+|---Laravel Middleware----
+|   What is middleware?
+|   Middleware Type
+|   Make Middleware
+|   Apply Middleware
 |   Interview Question
 |
-|--------------------------------------------------------------------------
+|---COMMAND ::: php artisan make:middleware checkAge------
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::post("users", [UsersController::class, 'getData']);
-Route::view("login", "users");
+// Route::post("users", [UsersController::class, 'getData']);
+Route::view("users", "users");
+Route::view("home", "home");
+Route::view("noaccess", "noaccess");
+
